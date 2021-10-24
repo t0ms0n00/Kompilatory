490f501c0d1081d9273e36f0a2bfc3984ae5b6db
Tomasz <tomaszboron2k@gmail.com>
2021-10-24 14:10:07 +0200

    add data types handlers

--- src/main.py
+++ src/main.py
@@ -1,66 +1,22 @@
-import ply.lex as lex
+import sys
+import scanner  # scanner.py is a file you create, (it is not an external library)
 
-tokens = ('PLUS',  'MINUS',  'TIMES',  'DIVIDE', 'LPAREN','RPAREN',  'LSQPAREN', 'RSQPAREN', 'LCPAREN', 'RCPAREN',
-          'NUMBER', 'ID', 'ASSIGN', 'ADDASSIGN', 'MULASSIGN', 'SUBASSIGN', 'DIVASSIGN', 'DOTADD', 'DOTSUB',
-          'DOTMUL', 'DOTDIV', 'LESS', 'GREATER', 'LESSEQUAL', 'GREATEREQUAL', 'NOTEQUAL', 'EQUAL')
+if __name__ == '__main__':
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
+    try:
+        filename = sys.argv[1] if len(sys.argv) > 1 else "./utils/text.txt"
+        file = open(filename, "r")
+    except IOError:
+        print("Cannot open {0} file".format(filename))
+        sys.exit(0)
 
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LSQPAREN = r'\['
-t_RSQPAREN = r'\]'
-t_LCPAREN = r'\{'
-t_RCPAREN = r'\}'
+    text = file.read()
+    lexer = scanner.lexer
+    lexer.input(text) # Give the lexer some input
 
-t_ASSIGN = r'='
-t_ADDASSIGN = r'\+='
-t_MULASSIGN = r'\*='
-t_SUBASSIGN = r'-='
-t_DIVASSIGN = r'/='
-
-t_DOTADD = r'\.\+'
-t_DOTMUL = r'\.\*'
-t_DOTSUB = r'\.-'
-t_DOTDIV = r'\./'
-
-t_LESS = r'\<'
-t_GREATER = r'\>'
-t_LESSEQUAL = r'\<='
-t_GREATEREQUAL = r'\>='
-t_NOTEQUAL = r'\!='
-t_EQUAL = r'\=='
-
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-
-def t_ID(t):
-    r'[a-zA-Z_]\w*'
-    return t
-
-
-t_ignore = '  \t'
-
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-
-def t_error(t) :
-    print("Illegal character '%s'" %t.value[0])
-    t.lexer.skip(1)
-
-
-lexer = lex.lex()
-fh = open("./utils/test1.txt", "r")
-lexer.input(fh.read())
-for token in lexer:
-    print("(%d): %s(%s)" %(token.lineno, token.type, token.value))
+    # Tokenize
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break    # No more input
+        print("(%d): %s (%s)" %(tok.lineno, tok.type, tok.value))
---
+++ src/scanner.py
@@ -0,0 +1,63 @@
+import ply.lex as lex
+
+tokens = ('INTEGER', 'FLOAT', 'STRING', 'ID', 'ADDASSIGN', 'MULASSIGN', 'SUBASSIGN', 'DIVASSIGN', 'DOTADD', 'DOTSUB',
+          'DOTMUL', 'DOTDIV', 'LESSEQUAL', 'GREATEREQUAL', 'NOTEQUAL', 'EQUAL')
+
+literals = "+-*/=<>(){}[]:',;"
+
+t_ADDASSIGN = r'\+='
+t_MULASSIGN = r'\*='
+t_SUBASSIGN = r'-='
+t_DIVASSIGN = r'/='
+
+t_DOTADD = r'\.\+'
+t_DOTMUL = r'\.\*'
+t_DOTSUB = r'\.-'
+t_DOTDIV = r'\./'
+
+t_LESSEQUAL = r'<='
+t_GREATEREQUAL = r'>='
+t_NOTEQUAL = r'!='
+t_EQUAL = r'=='
+
+t_ignore = '  \t'
+
+
+def t_STRING(t):
+    r'".*"'
+    return t
+
+
+def t_FLOAT(t):
+    r'((\d*\.\d+)|(\d+\.\d*))((E|e)(\+|-)?\d+)?'        # żeby nie łapało samej . i pozwalało na 0. i .0
+    t.value = float(t.value)
+    return t
+
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+
+def t_ID(t):
+    r'[a-zA-Z_]\w*'
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
