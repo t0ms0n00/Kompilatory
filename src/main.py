17d13d9d579cb77c2a5b1f449f20cbf3ece69de8
Tomasz <tomaszboron2k@gmail.com>
2021-12-16 08:45:19 +0100

    lab5 init

--- src/main.py
+++ src/main.py
@@ -1,22 +1,55 @@
+# import sys
+# import scanner
+# import Mparser
+# from TypeChecker import TypeChecker
+#
+# if __name__ == '__main__':
+#
+#     try:
+#         filename = sys.argv[1] if len(sys.argv) > 1 else "utils/test.m"
+#         file = open(filename, "r")
+#     except IOError:
+#         print("Cannot open {0} file".format(filename))
+#         sys.exit(0)
+#
+#     parser = Mparser.parser
+#     text = file.read()
+#
+#     ast = parser.parse(text, lexer=scanner.lexer)
+#
+#     # Below code shows how to use visitor
+#     typeChecker = TypeChecker()
+#     typeChecker.visit(ast)   # or alternatively ast.accept(typeChecker)
+
 import sys
-import scanner
-import Mparser
+import ply.yacc as yacc
+from Mparser import Mparser
+from TreePrinter import TreePrinter
 from TypeChecker import TypeChecker
+from Interpreter import Interpreter
+
 
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "utils/test.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
         sys.exit(0)
 
-    parser = Mparser.parser
+    Mparser = Mparser()
+    parser = yacc.yacc(module=Mparser)
     text = file.read()
 
-    ast = parser.parse(text, lexer=scanner.lexer)
+    ast = parser.parse(text, lexer=Mparser.scanner)
 
     # Below code shows how to use visitor
     typeChecker = TypeChecker()
     typeChecker.visit(ast)   # or alternatively ast.accept(typeChecker)
+
+    ast.accept(Interpreter())
+    # in future
+    # ast.accept(OptimizationPass1())
+    # ast.accept(OptimizationPass2())
+    # ast.accept(CodeGenerator())
